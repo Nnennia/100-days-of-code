06ad817a2da2a04f003913a9ec3dd183038b586a
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-09-13 02:08:41 +0100

    commit

---
+++ 0x09-PYTHON/blind_auction.py
@@ -0,0 +1,27 @@
+import os
+from art import logo
+
+print(logo)
+data_bidding = {}
+
+
+clear = True
+
+while clear != False:
+
+	name = input("What is your name?\n")
+	bid = int(input("How much are you going to bid?\n"))
+	data_bidding[name] = bid
+	clear = input("Are there others in the room? Y or N\n").lower()
+
+	if clear == "y":
+		flag = False
+		os.system("cls")
+		#print(data_bidding)
+	elif clear == "n":
+		clear = False
+		os.system("cls")
+max_key = max(data_bidding)
+max_value = max(data_bidding.values())
+print(f"The winner is {max_key} with a bid of $ {max_value}")
+
