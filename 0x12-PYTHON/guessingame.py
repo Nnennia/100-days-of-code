b814b0d4444ae4afa39c80e77c3447ccd57b137c
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-09-16 08:37:23 +0100

    commit

---
+++ 0x12-PYTHON/guessingame.py
@@ -0,0 +1,59 @@
+import random
+
+random_number = random.randrange(0,101,1)
+print("Welcome to the number guessing game!\nI'm thinking of a number between 1 and 100.")
+difficulty = input("Choose a difficulty\nType 'easy' or 'hard' ").lower()
+
+if difficulty == "easy":
+	attempts = 10
+
+	print(f"You have {attempts} remaining to guess the number.")
+	flag = True
+	while flag != False:
+		user = int(input("Make a guess: "))
+		if user != random_number and user > random_number:
+			flag = True
+			attempts -= 1
+			print("Too high")
+			print (f"{attempts} attempts left to guess the number.\nGuess again.")
+			if attempts == 0:
+				flag = False
+				print(f"The random number is {random_number}")
+		elif user < random_number:
+			flag = True
+			print("Too low")
+			attempts -= 1
+			print(f"You have {attempts} left.\nGuess again.")
+			if attempts == 0:
+				flag = False
+				print(f"You lose. Random number was {random_number}")
+		elif user == random_number:
+			print(f"You win. You number was {user} and random number was {random_number}")
+		else:
+			print("Input a number")
+elif difficulty == "hard":
+	attempts = 5
+	print(f"You have {attempts} remainning to guess the number.")
+	flag = True
+	while flag != False:
+		user = int(input("Make a guess: "))
+		if user != random_number and user > random_number:
+			flag = True
+			attempts -= 1
+			print("Too high")
+			print (f"{attempts} attempts left to guess the number.\nGuess again.")
+			if attempts == 0:
+				flag = False
+				print(f"The random number is {random_number}")
+		elif user < random_number:
+			flag = True
+			print("Too low")
+			attempts -= 1
+			print(f"You have {attempts} left.\nGuess again.")
+			if attempts == 0:
+				flag = False
+				print(f"You lose. Random number was {random_number}")
+		elif user == random_number:
+			print(f"You win. You number was {user} and random number was {random_number}")
+		else:
+			print("Input a number")
