1da332820b46fff2cffe88ffebd76b0e174c7620
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-09-20 10:54:28 +0100

    commit

---
+++ 0x016-PYTHON/main.py
@@ -0,0 +1,10 @@
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Nnennia", ["Nnenna", "Chioma", "Adaeze"])
+table.add_column("Atuanya", ["Adaese", "rerghr", "jbuuf"])
+
+table.align = "l"
+
+
+print(table)
