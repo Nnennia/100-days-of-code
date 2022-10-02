c15018ca8f4c72856c2744938907dd291a30fbbf
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-10-02 13:45:17 +0100

    commit

---
+++ 0x17-PYTHON/main.py
@@ -0,0 +1,15 @@
+from quiz_brain import QuizBrain
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+quiz.still_has_questions()
+#quiz.next_question()
