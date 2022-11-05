c6d3a2fa2de83e96b6e60e2b92c066bb3256e6fe
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-11-05 17:26:17 +0100

    commit

--- 23-day/main.py
+++ 23-day/main.py
@@ -1,3 +1,15 @@
-with open("weather_data.csv") as data_file:
-    data = data_file.readlines()
-    print(data)
+# comma seperated value
+
+import pandas
+
+data = pandas.read_csv("23-day/228 2018-Central-Park-Squirrel"
+                       "-Census-Squirrel-Data.csv")
+gray = len(data[data["Primary Fur Color"] == "Gray"])
+black = len(data[data["Primary Fur Color"] == "Black"])
+Cin = len(data[data["Primary Fur Color"] == "Cinnamon"])
+
+dict = {"Fur Color": ["Gray", "Cinnamon", "Black"],
+        "Count": [gray, black, Cin]}
+df = pandas.DataFrame(dict)
+
+df.to_csv("23-day/squ.csv")
